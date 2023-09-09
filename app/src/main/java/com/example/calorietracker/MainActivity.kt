4bcc4ff9227008843a7cacade4a353ecf661888f
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2023-09-09 18:37:22 +0100

    Age Screen

--- app/src/main/java/com/example/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/MainActivity.kt
@@ -1,8 +1,12 @@
-package com.example.calorietracker
+package com.example.calorieTracker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,59 +17,69 @@ import androidx.navigation.compose.rememberNavController
 import com.example.calorietracker.navigation.navigate
 import com.example.calorietracker.ui.theme.CalorieTrackerTheme
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalorieTrackerTheme {
+            CalorieTrackerTheme(darkTheme = false) {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
-                ) {
-                    composable(Route.WELCOME) {
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) {padding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
 
-                    composable(Route.AGE) {
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
 
-                    }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
 
-                    composable(Route.GENDER) {
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
+                        composable(Route.HEIGHT) {
 
-                    composable(Route.HEIGHT) {
+                        }
 
-                    }
+                        composable(Route.WEIGHT) {
 
-                    composable(Route.WEIGHT) {
+                        }
 
-                    }
+                        composable(Route.NUTRIENT_GOAL) {
 
-                    composable(Route.NUTRIENT_GOAL) {
+                        }
 
-                    }
+                        composable(Route.ACTIVITY) {
 
-                    composable(Route.ACTIVITY) {
+                        }
 
-                    }
+                        composable(Route.GOAL) {
 
-                    composable(Route.GOAL) {
+                        }
 
-                    }
+                        composable(Route.TRACKER_OVERVIEW) {
 
-                    composable(Route.TRACKER_OVERVIEW) {
+                        }
 
-                    }
+                        composable(Route.SEARCH) {
 
-                    composable(Route.SEARCH) {
-
+                        }
                     }
                 }
 
